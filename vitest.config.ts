4dcbd5ae783650ff1ce8ba6a7d46ca421ebfa6f5
Sniper Ravan <akashdasdhibar2003@gmail.com>
2026-07-21 23:04:11 +0530

    test: exclude references directory from vitest runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default defineConfig({
   test: {
     setupFiles: ['./tests/setup.ts'],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/extension.spec.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/extension.spec.ts', '**/references/**'],
     alias: {
       '@': resolve(__dirname, './src'),
     },
